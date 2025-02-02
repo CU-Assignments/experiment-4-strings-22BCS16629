b8afab741c96cfaa336be7cf029c182ad7585207
22BCS16629 <22BCS16629@cuchd.in>
2025-02-02 22:43:04 +0530

    Add files via upload

---
+++ StringShift.java
@@ -0,0 +1,15 @@
+public class StringShift {
+    public static boolean canShift(String s, String goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        String concatenated = s + s;
+       return concatenated.contains(goal);
+    }
+    public static void main(String[] args) {
+        String s = "abcde";
+        String goal = "cdeab";
+        boolean result = canShift(s, goal);
+        System.out.println(result);
+    }
+}
